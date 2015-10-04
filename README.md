aa107633597edf112a2e31fc70255f1fc59d6f1b
wwalser <waw325@gmail.com>
2015-10-04 21:36:10 +1100

    Fresh start for nearly-new thing.

---
+++ README.md
@@ -0,0 +1,5 @@
+# embeddable-cors
+
+This is a fork of [Tiago Quelhas's corsRequest](https://github.com/tjgq/cors-request) with features removed for use as an embedded CORs library. I wouldn't recommend using it. If you need a minimal cors lib, upstream will probably keep you more comfortable.
+
+There is a minified version inside of the build directory for convenience.
---
+++ embeddable-cors.js
@@ -0,0 +1,97 @@
+cors = (function() {
+
+  // Determine the underlying implementation to be used.
+  var useXhr = 'withCredentials' in new XMLHttpRequest();
+  var useXdr = !useXhr && typeof XDomainRequest !== 'undefined';
+  var Request;
+  if (useXhr) {
+    Request = XMLHttpRequest;
+  } else {
+    Request = XDomainRequest;
+  }
+
+  // A function that does nothing.
+  var noop = function() {};
+
+  // Perform and store state for an HTTP request.
+  function CorsRequest(method, url, data, done) {
+
+    var self = this;
+
+    function error(msg) {
+      return new Error('corsRequest: ' + method + ' ' + url + ': ' + msg);
+    }
+
+    this.method = method;
+    this.url = url;
+    this.data = data;
+
+    this.req = new Request();
+
+    this.req.open(this.method, this.url);
+
+    this.req.onerror = function() {
+      self.err = error('error');
+      done(self.err);
+    };
+
+    this.req.onload = function() {
+      if (useXhr && self.req.readyState !== 4) {
+        // XMLHttpRequest not yet complete.
+        return;
+      }
+      if (useXhr && self.req.status === 0) {
+        // XMLHttpRequest aborted, e.g. CORS error or connection reset.
+        self.err = error('incomplete');
+        done(self.err);
+        return;
+      }
+      // Request complete.
+      self.err = null;
+      self.res = {
+        status: useXhr ? self.req.status : 200,
+        text: self.req.responseText,
+        json: JSON.parse(self.req.responseText)
+      };
+      done(self.err, self.res);
+    };
+
+    if (this.data) {
+      var payload = JSON.stringify(data);
+    }
+
+    self.req.send(payload || null);
+  }
+
+
+  // Instantiate CorsRequest with default arguments.
+  function corsRequest() {
+
+    var method, url, data, done;
+
+    // corsRequest(method, url, ...)
+    method = arguments[0].toUpperCase();
+    url = arguments[1];
+
+    if (arguments.length === 3) {
+      if (typeof arguments[2] === 'function') {
+        // corsRequest(method, url, done)
+        done = arguments[2];
+      } else {
+        // corsRequest(method, url, data)
+        data = arguments[2];
+      }
+    }
+
+    if (arguments.length > 3) {
+      // corsRequest(method, url, data, done)
+      data = arguments[2];
+      done = arguments[3];
+    }
+
+    return new CorsRequest(method, url, data, done || noop);
+  }
+
+  // Export public interface.
+  return corsRequest;
+})();
